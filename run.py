4506b573fa260ae883a0a3cce43eefdf7c2f89bc
MinaXu1314 <303139496@qq.com>
2020-11-01 15:06:49 +0800

    更新run.py

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from config import Conf
 if __name__ == '__main__':
     report_path = Conf.get_report_path() + os.sep + "result"
     report_html_path = Conf.get_report_path() + os.sep + "html"
-    pytest.main(["-s", "--alluredir", report_path])
+    pytest.main(["-s", "test_excel_case_v7.py", "--alluredir", report_path])
